f2b4583098097bcfdab651f054f4d2762c120fbb
ismetkarakus <ismetkarakus1@hotmail.com>
2026-03-03 00:44:29 +0000

    Make order_items lot FK migration idempotent

--- apps/api/src/db/migrations/0018_order_items_lot_based.sql
+++ apps/api/src/db/migrations/0018_order_items_lot_based.sql
@@ -7,9 +7,19 @@ DROP CONSTRAINT IF EXISTS order_items_food_id_fkey;
 ALTER TABLE order_items
 DROP CONSTRAINT IF EXISTS order_items_order_id_food_id_key;
 
-ALTER TABLE order_items
-ADD CONSTRAINT order_items_lot_id_fkey
-FOREIGN KEY (lot_id) REFERENCES production_lots(id) ON DELETE RESTRICT;
+DO $$
+BEGIN
+  IF NOT EXISTS (
+    SELECT 1
+    FROM pg_constraint
+    WHERE conname = 'order_items_lot_id_fkey'
+  ) THEN
+    ALTER TABLE order_items
+    ADD CONSTRAINT order_items_lot_id_fkey
+    FOREIGN KEY (lot_id) REFERENCES production_lots(id) ON DELETE RESTRICT;
+  END IF;
+END
+$$;
 
 DO $$
 BEGIN
