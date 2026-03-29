a16680c7c080843feb2ac86560374f2833d63678
ismetkarakus <ismetkarakus1@hotmail.com>
2026-03-29 00:40:16 +0000

    mobile: tweak foods manager title size down

--- apps/mobile/src/screens/SellerFoodsManagerScreen.tsx
+++ apps/mobile/src/screens/SellerFoodsManagerScreen.tsx
@@ -185,7 +185,7 @@ const styles = StyleSheet.create({
   pageTitle: {
     color: "#2E241C",
     fontWeight: "900",
-    fontSize: 21,
+    fontSize: 20,
     textAlign: "center",
     marginTop: 8,
     marginBottom: 2,
