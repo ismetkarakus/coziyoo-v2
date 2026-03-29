393c2a597ce7a67a3eb9935e37806906a770bb4d
ismetkarakus <ismetkarakus1@hotmail.com>
2026-03-29 00:42:18 +0000

    mobile: make add-food title slightly larger than edit title

--- apps/mobile/src/screens/SellerFoodsManagerScreen.tsx
+++ apps/mobile/src/screens/SellerFoodsManagerScreen.tsx
@@ -181,7 +181,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     paddingHorizontal: 14,
   },
-  addBtnText: { color: "#fff", fontWeight: "800", fontSize: 15 },
+  addBtnText: { color: "#fff", fontWeight: "800", fontSize: 21 },
   pageTitle: {
     color: "#2E241C",
     fontWeight: "900",
